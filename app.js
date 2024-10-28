dcb41e41784e40d8eaa39e48a2b1c4bcfc768445
Shyamal <info@bigsss.com>
2024-10-28 15:28:35 +0530

    updated

--- app.js
+++ app.js
@@ -15,9 +15,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
-app.get("/", (req, res, next) => {
-  res.send("Welcome");
-});
+
 app.use("/api", userRouter);
 
 app.use((error, req, res, next) => {
